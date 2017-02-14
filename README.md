7eca99447c4d86db8372b82a3fd50af44a5a8b78
Jeoffrey Bauvin <jeoffrey54@gmail.com>
2017-02-14 19:20:29 +0100

    Rename stuff

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ $ virtualenv -p python3 venv
 $ . venv/bin/activate
 $ pip install -r requirements.txt
 ~~~
+
+# Stuff to know about Puppet's ENC
+
+https://docs.puppet.com/guides/external_nodes.html#enc-output-format
---
+++ app/puppenc.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from flask import Flask, Blueprint
+from flask_restful import Resource, Api, reqparse
+from flask_sqlalchemy import SQLAlchemy
+
+WHOAMI='puppenc'
+
+app = Flask(WHOAMI)
+
+api_bp = Blueprint('api', WHOAMI)
+
+app.config.from_object('config')
+
+api = Api(api_bp, prefix=app.config['PREFIX'])
+db = SQLAlchemy(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('as_yaml', type=int, default=0)
+
+from app.objects.routes import Objects
+
+class Index(Resource):
+    def get(self):
+        return 'Welcome to Puppenc'
+
+api.add_resource(Index, '/', '/index')
+app.register_blueprint(api_bp)
