2aeb23ce37226f3a8b4a2b9d573d2d316cab886f
Jeoffrey Bauvin <jeoffrey.bauvin.ext@boursorama.fr>
2017-02-24 16:19:36 +0100

    Add nodes

---
+++ app/nodes/models.py
@@ -0,0 +1,22 @@
+from app.puppenc import db
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref
+
+
+class Node(db.Model):
+    __tablename__ = 'nodes'
+    id             = db.Column(db.Integer, primary_key=True)
+    name           = db.Column(db.String(255))
+    insert_date    = db.Column(db.DateTime, default=db.func.current_timestamp())
+    update_date    = db.Column(db.DateTime, default=None)
+    delete_date    = db.Column(db.DateTime, default=None)
+    hostgroup_id   = db.Column(db.Integer, db.ForeignKey('hostgroups.id'))
+    environment_id = db.Column(db.Integer, db.ForeignKey('environments.id'))
+    active         = db.Column(db.Integer, default=1)
+
+    def __init__(self, name, environment_id, hostgroup_id):
+        self.name = name
+        self.environment_id = environment_id
+        self.hostgroup_id = hostgroup_id
+
+    def __repr__(self):
+        return '<Node %r>' % (self.name)
---
+++ app/nodes/schema.py
@@ -0,0 +1,8 @@
+from app.puppenc import ma
+from app.nodes.models import Node
+
+class NodeSchema(ma.ModelSchema):
+    class Meta:
+        model = Node
+        # Fields to expose
+        fields = ('id', 'name', 'insert_date', 'update_date', 'delete_date', 'hostgroup_id', 'environment_id', 'active')
