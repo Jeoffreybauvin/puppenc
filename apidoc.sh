e87c856039a1f1ae59f4ebd79be1b8e47ec9dcc5
Jeoffrey Bauvin <jeoffrey.bauvin.ext@boursorama.fr>
2017-03-31 16:01:03 +0200

    Apidoc.sh

--- apidoc.sh
+++ apidoc.sh
@@ -1,1 +1,1 @@
-apidoc -i app/classes/ -i app/enc/ -i app/environments/ -i app/hostgroups/  -i app/nodes/  -o apidoc/
+apidoc -i app/classes/ -i app/enc/ -i app/environments/ -i app/hostgroups/  -i app/nodes/  -i app/users/ -o apidoc/
