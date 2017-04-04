537bf1452fadba8491eab36cb886be5123524941
Jeoffrey Bauvin <jeoffrey.bauvin.ext@boursorama.fr>
2017-04-04 18:52:07 +0200

    Add setup arg

--- shell.py
+++ shell.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
-import os
+import os, sys
 import readline
 from pprint import pprint
-
 from flask import *
 from app import puppenc
-
 os.environ['PYTHONINSPECT'] = 'True'
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("setup", help="do the initial setup")
+args = parser.parse_args()
+if args.setup:
+    puppenc.init_db()
+    sys.exit()
