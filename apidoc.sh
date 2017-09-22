bb31e5f49e39e2e29f8342971fae6a793efbdbcf
Jeoffrey Bauvin <jeoffrey.bauvin.ext@boursorama.fr>
2017-09-22 14:18:22 +0200

    Delete hostgroups, so update docs

--- apidoc.sh
+++ apidoc.sh
@@ -1,1 +1,1 @@
-apidoc -i app/classes/ -i app/enc/ -i app/environments/ -i app/hostgroups/  -i app/nodes/  -i app/users/ -i app/variables/ -o docs/
+apidoc -i app/classes/ -i app/enc/ -i app/environments/ -i app/nodes/  -i app/users/ -i app/variables/ -o docs/
