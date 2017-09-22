a2ccb15971aa2c1cc7646951144dcd0d5d642516
Jeoffrey Bauvin <jeoffrey.bauvin.ext@boursorama.fr>
2017-09-22 14:17:54 +0200

    Remove hostgroups part

--- app/hostgroups/schema.py
+++
@@ -1,8 +0,0 @@
-from app.puppenc import ma
-from app.hostgroups.models import Hostgroup
-
-class HostgroupSchema(ma.ModelSchema):
-    class Meta:
-        model = Hostgroup
-        # Fields to expose
-        fields = ('id', 'name', 'insert_date', 'update_date', 'class_id', 'nodes')
